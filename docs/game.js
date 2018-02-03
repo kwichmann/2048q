15ac5256149828e3bc163304dea7f2a061224db7
kwichmann <kristian.wichmann@gmail.com>
2018-02-03 16:33:12 +0100

    Greedy agent implemented

--- docs/game.js
+++ docs/game.js
@@ -1,27 +1,91 @@
 let scoreDiv = document.getElementById("score");
 let score;
+
 let moves;
+
+let agentDiv = document.getElementById("agent");
+let agent = "Human";
 
 newGame();
 
 document.addEventListener('keydown', function(event) {
-  let move = -1;
-  if(event.keyCode == 78) {
+  if (event.keyCode == 78) {
     newGame();
     return undefined;
-  } else if(event.keyCode == 39) {
-    move = 0;
-  } else if(event.keyCode == 38) {
-    move = 1;
-  } else if(event.keyCode == 37) {
-    move = 2;
-  } else if(event.keyCode == 40) {
-    move = 3;
   }
-  if (move == -1) {
+
+  if (event.keyCode == 72) {
+    agent = "Human";
+    showAgent();
     return undefined;
   }
-  move = moves[move];
+
+  if (event.keyCode == 71) {
+    agent = "Greedy";
+    showAgent();
+    return undefined;
+  }
+
+  if (agent != "Human") {
+    return undefined;
+  }
+
+  // Human agent
+  if (event.keyCode == 39) {
+    makeMove(0);
+  } else if(event.keyCode == 38) {
+    makeMove(1);
+  } else if(event.keyCode == 37) {
+    makeMove(2);
+  } else if(event.keyCode == 40) {
+    makeMove(3);
+  }
+});
+
+// Game loop (for non-human agents)
+setInterval(function() {
+  if (agent == "Greedy") {
+    let maxScore = -1;
+    let maxMoves = [];
+
+    for (let i = 0; i < 4; i++) {
+      if (!(moves[i]["deadlock"])) {
+        let s = moves[i]["score"];
+        if (s == maxScore) {
+          maxMoves.push(i);
+        }
+        if (s > maxScore) {
+          maxMoves = [i];
+          maxScore = s;
+        }
+      }
+    }
+
+    if (maxMoves.length == 0) {
+      return undefined;
+    }
+
+    let m = maxMoves[Math.floor(Math.random() * maxMoves.length)];
+    makeMove(m);
+  }
+}, 50);
+
+function newGame() {
+  score = 0;
+  showScore();
+  showAgent();
+
+  for (let i = 0; i < 16; i++) {
+    setSquare(i, -1);
+  }
+  setRandom();
+  setRandom();
+
+  moves = getMoves(powerGrid);
+}
+
+function makeMove(m) {
+  let move = moves[m];
   if (move["deadlock"]) {
      return undefined;
   }
@@ -33,23 +97,14 @@ document.addEventListener('keydown', function(event) {
   moves = getMoves(powerGrid);
 
   checkGameOver();
-});
-
-function newGame() {
-  score = 0;
-  showScore();
-
-  for (let i = 0; i < 16; i++) {
-    setSquare(i, -1);
-  }
-  setRandom();
-  setRandom();
-
-  moves = getMoves(powerGrid);
 }
 
 function showScore() {
   scoreDiv.innerHTML = "Score: " + score;
+}
+
+function showAgent() {
+  agentDiv.innerHTML = "Agent: " + agent;
 }
 
 function checkGameOver() {
