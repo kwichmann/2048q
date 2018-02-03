0de1ebed7a0ab9cc372f69a36bc703f1259c8480
kwichmann <kristian.wichmann@gmail.com>
2018-02-03 05:28:28 +0100

    Grid logic done

--- docs/grid.js
+++ docs/grid.js
@@ -99,10 +99,98 @@ function combine(rowObject) {
   }
 }
 
+function stripRow(rawRow) {
+  return rawRow.reduce(function(soFar, current) {
+    if (current == -1) {
+      return soFar;
+    }
+    soFar.push(current);
+    return soFar;
+  }, []);
+}
+
 function padRow(combined) {
   if (combined.length < 4) {
     return Array(4 - combined.length).fill(-1).concat(combined);
   } else {
     return combined;
   }
+}
+
+function moveRow(row) {
+  row = stripRow(row);
+  let combined = combine({
+    row: row,
+    score: 0
+  });
+  combined["row"] = padRow(combined["row"]);
+  return combined;
+}
+
+function moveRight(g) {
+  let newCombined = [
+    moveRow([g[0], g[1], g[2], g[3]]),
+    moveRow([g[4], g[5], g[6], g[7]]),
+    moveRow([g[8], g[9], g[10], g[11]]),
+    moveRow([g[12], g[13], g[14], g[15]])
+  ];
+  return {
+    grid: newCombined.reduce(function(g, row) {
+      return g.concat(row["row"]);
+    }, []),
+    score: newCombined.reduce(function(s, row) {
+      return s + row["score"];
+    }, 0)
+  };
+}
+
+// Rotate grid 90 degrees clockwise
+function rotateGrid(g) {
+  return [
+    g[12], g[8], g[4], g[0],
+    g[13], g[9], g[5], g[1],
+    g[14], g[10], g[6], g[2],
+    g[15], g[11], g[7], g[3]
+  ];
+}
+
+// Rotate grid 90 degrees counterclockwise
+function rotateGridCounter(g) {
+  return [
+    g[3], g[7], g[11], g[15],
+    g[2], g[6], g[10], g[14],
+    g[1], g[5], g[9], g[13],
+    g[0], g[4], g[8], g[12]
+  ];
+}
+
+// Rotate grid 180 degrees
+function rotateGrid180(g) {
+  g.reverse();
+  return g;
+}
+
+function getMoves(g) {
+  // Right
+  let moves = [moveRight(g)];
+
+  // Up
+  g = rotateGrid(g);
+  let up = moveRight(g);
+  up["grid"] = rotateGridCounter(up["grid"]);
+  moves.push(up);
+
+  // Left
+  g = rotateGrid(g);
+  let left = moveRight(g);
+  left["grid"] = rotateGrid180(left["grid"]);
+  moves.push(left);
+
+  // Down
+  g = rotateGrid(g);
+  let down = moveRight(g);
+  down["grid"] = rotateGrid(down["grid"]);
+  moves.push(down);
+
+  return moves;
 }
